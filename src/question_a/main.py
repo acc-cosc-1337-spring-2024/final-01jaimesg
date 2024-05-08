9ef0b9b6ff1bbcc9e706c24c43fea2517d24599e
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-05-08 23:18:41 +0000

    Question_a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,18 @@
-#add import
+largest = None
+smallest = None
+List_collector=[]
+while True:
+    num = input("Enter 5 number and when complete type 'done'.Enter a number: ")
+    if num == 'done':
+        break
+    n = int(num)
+    List_collector.append(n)
+    largest=max(List_collector)
+    smallest=min(List_collector)
+    
+    #print("Invalid input")
+    continue
+print("Maximum number is", largest)
+print("Minimum number is", smallest)
+print("Average number is", sum(List_collector) / len(List_collector))
+print("The sum of all numbers is", sum(List_collector))
